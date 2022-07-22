1fa6d4a0c83e865b99f250ba567754fe3ae93374
Mukti <mukti.devmail@gmail.com>
2022-07-22 14:38:43 +0700

    Update small changes to login and admin panel design.

--- app/Http/Controllers/Landing.php
+++ app/Http/Controllers/Landing.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class Landing extends Controller
 {
     public function index(){
-        return view('landing.landing');
+        return view('landing.landing', ['pagename' => 'Homepage']);
+    }
+
+    public function projects(){
+        return view('landing.project', ['pagename' => 'Our Work']);
     }
 }
