99c3169f252e308f8bb251abe3c7613243045a07
Mukti <mukti.devmail@gmail.com>
2022-07-25 11:15:31 +0700

    add role field to user system.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,12 @@
 <li class="nav-header"># Content Management</li>
-@if(Auth::user()->role=='admin') 
-<li class="nav-item">
+@if(Auth::user()->role=='admin')
+
+<!-- <li class="nav-item">
     <a href="{{ route('projects.index') }}"
        class="nav-link {{ Request::is('projects*') ? 'active' : '' }}">
         <p>Project</p>
     </a>
-</li>
+</li> -->
 
 
 @endif
