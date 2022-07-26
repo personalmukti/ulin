5ac2a733984b72c7d3898c84e40d1ad22e06d8c6
feryx26 <105197714+feryx26@users.noreply.github.com>
2022-07-26 10:35:42 +0700

    rollback perencanaan

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,5 @@
 <li class="nav-header"># Content Management</li>
 
-<li class="nav-item">
-    <a href="{{ route('perencanaans.index') }}"
-       class="nav-link {{ Request::is('perencanaans*') ? 'active' : '' }}">
-        <p>Perencanaan</p>
-    </a>
-</li>
 
 @if(Auth::user()->role=='admin')
 <li class="nav-item">
