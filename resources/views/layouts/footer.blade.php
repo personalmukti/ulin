e242380f4d90ef2e0938a861d29e35e330713c26
Mukti <mukti.devmail@gmail.com>
2022-07-27 09:27:30 +0700

    Edit footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
         <div class="float-right d-none d-sm-block">
             <b>Version</b> 1.0.0
         </div>
-        <strong>Copyright &copy; <script>document.write(/\d{4}/.exec(Date())[0])</script> <a href="https://adminlte.io">{{ config('app.name') }}</a>.</strong> All rights
+        <strong>Copyright &copy; <script>document.write(/\d{4}/.exec(Date())[0])</script> <a href="/home">{{ config('app.name') }}</a>.</strong> All rights
         reserved.
     </footer>
